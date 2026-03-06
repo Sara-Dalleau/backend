54888fd1b3ddad1a72c6f736c97b78a5bd8633a2
Dalleau Sara <sara.dalleau@gmail.com>
2026-03-06 16:04:50 +0100

    configuration CORS et création des premières routes API /api/books

--- app.js
+++ app.js
@@ -2,8 +2,49 @@ const express = require('express'); // On importe express depuis node_modules.
 
 const app = express(); // On créer une app express.
 
+// Middleware qui permet de lire le JSON envoyé dans les requêtes
+app.use(express.json());
+
+// Middleware CORS : permet au frontend (localhost:4200) de communiquer avec le backend (localhost:3000)
 app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next(); // on passe au middleware suivant
 });
 
-module.exports = app;//export de l'app pour pouvoir y accéder depuis autres fichiers.
+// ROUTE GET : récupérer les livres
+app.use('/api/books', (req, res, next) => {
+
+  const books = [
+    {
+      _id: "1",
+      title: "Premier livre",
+      author: "Auteur",
+      imageUrl: "https://picsum.photos/200",
+      year: 2023,
+      genre: "Roman",
+      ratings: []
+    }
+  ];
+  res.status(200).json(books);
+});
+
+// ROUTE POST : créer un livre
+app.post('/api/books', (req, res, next) => {
+  console.log(req.body); // affiche les données envoyées par le frontend
+
+  res.status(201).json({
+    message: 'Livre créé !'
+  });
+
+});
+
+module.exports = app; //export de l'app pour pouvoir y accéder depuis autres fichiers.
